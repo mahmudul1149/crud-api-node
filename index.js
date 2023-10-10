4ba35257d6cfcd0bb9c68bff436a262c097e2a30
mahmudul <imdmahmudul237@gmail.com>
2023-10-10 13:46:24 +0600

    Add get and post api

--- index.js
+++ index.js
@@ -5,6 +5,53 @@ const client = require("./connect-mogodb");
 
 app.use(cors());
 app.use(express.json());
+app.get("/todo/all", async (req, res) => {
+  try {
+    await client.connect();
+    const todos = await client
+      .db("todos-collection")
+      .collection("todos")
+      .find({})
+      .toArray();
+    res.send({
+      success: true,
+      todos,
+    });
+  } catch (error) {
+    console.error("Error in /todo/all:", error);
+    res.status(500).send({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+});
+
+app.post("/todo", async (req, res) => {
+  try {
+    await client.connect();
+
+    const todoCount = await client
+      .db("todos-collection")
+      .collection("todos")
+      .countDocuments({});
+
+    const todo = req.body;
+    todo.id = todoCount + 1;
+
+    await client.db("todos-collection").collection("todos").insertOne(todo);
+
+    res.send({
+      success: true,
+      todo,
+    });
+  } catch (error) {
+    console.error("Error in /todo:", error);
+    res.status(500).send({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+});
 
 const port = 3000;
 
